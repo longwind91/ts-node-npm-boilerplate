b502d8cb00b65562af9e48b8ef419032869d8ac6
lichangfeng1 <lichangfeng1@xiaomi.com>
2023-06-19 09:49:14 +0800

    feat: add npm publish and test file

---
+++ test/test_in_your_program.js
@@ -0,0 +1,6 @@
+// test in your program
+const package = require("ts-node-npm-boilerplate")
+const helloInstance = new package.Hello('longwind')
+helloInstance.hi()
+helloInstance.hi()
+helloInstance.hi()
